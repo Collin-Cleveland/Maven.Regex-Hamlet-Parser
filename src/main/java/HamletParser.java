c1fe0c5ee2219bb739e2832631c8e8bc4ae98652
Collin <collin@Zipcoders-MBP.hsd1.de.comcast.net>
2022-12-10 20:44:12 -0500

    create 4 methods for tests and tests passing

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -32,8 +34,40 @@ public class HamletParser {
         return result.toString();
     }
 
-    public String getHamletData(){
-        return hamletData;
+    public String getHamletData(){return hamletData;}
+
+    public boolean findHamlet(String hamletData) {
+        Pattern pHam = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher mHam = pHam.matcher(hamletData);
+        return mHam.find();
+    }
+
+    public boolean findHoratio(String hamletData) {
+        Pattern pHor = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher mHor = pHor.matcher(hamletData);
+        return mHor.find();
+    }
+
+    public StringBuffer replaceHamlet() {
+        StringBuffer sb = new StringBuffer();
+        Pattern pHam = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher mHam = pHam.matcher(hamletData);
+
+        while(mHam.find()){
+            mHam.appendReplacement(sb, "Leon");
+        }
+        return sb;
+    }
+
+    public StringBuffer replaceHoratio() {
+        StringBuffer sb = new StringBuffer();
+        Pattern pHam = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher mHam = pHam.matcher(hamletData);
+
+        while(mHam.find()){
+            mHam.appendReplacement(sb, "Tariq");
+        }
+        return sb;
     }
 
 }
